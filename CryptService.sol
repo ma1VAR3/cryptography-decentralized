51fceb17ebde7ca881a4f59d055cefda4fea1002
ma1VAR3 <prajjwalgupta1465@gmail.com>
2021-09-28 20:25:01 +0530

    creating contract

--- CryptService.sol
+++ CryptService.sol
@@ -4,4 +4,14 @@ pragma solidity >=0.5.0 <0.7.0;
 
 contract CryptService {
     
+    int counter;
+
+    struct cipherAssociation {
+        string identifier;
+        string cipher;
+        address issuer;
+        address[] accessPool;
+    }
+
+    cipherAssociation[] CA;
 }
